8ab82982189a262982a41c718bdcff58ed053140
SvetlanaK <64019362+SvetlanaRT@users.noreply.github.com>
2022-11-29 00:23:25 +0200

    Update conftest.py
    update conftest

--- conftest.py
+++ conftest.py
@@ -19,11 +19,17 @@ def setup(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument('--headless')
+    
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
     #----------------------------------------------------------------------------
     
     options.set_capability("acceptInsecureCerts", True)
-    # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
-    driver = webdriver.Chrome(executable_path="/home/svetlanakalchenko/PycharmProjects/BEautomation/chromedriver",chrome_options=options)
+    driver = webdriver.Chrome(chrome_options=options)
+
+    #driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
+    #driver = webdriver.Chrome(executable_path="/home/svetlanakalchenko/PycharmProjects/BEautomation/chromedriver",chrome_options=options)
+    
     # --------------------------------------------------------------------------
 
 
