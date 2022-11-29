b5dc42b9625197ba55801029f9909d41c7c6f5d8
SvetlanaK <64019362+SvetlanaRT@users.noreply.github.com>
2022-11-29 10:17:15 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -18,7 +18,7 @@ def setup(request):
     #driver solution------------------------------------------------------------
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
