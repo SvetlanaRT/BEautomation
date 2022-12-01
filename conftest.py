f1a30c598507336a28102fc1eed6071fe1fb41b2
svetlanakalchenko <adlinsvetlana@gmail.com>
2022-12-02 00:54:17 +0200

    test.kaymera

--- conftest.py
+++ conftest.py
@@ -1,43 +1,50 @@
 import json
-
 import pytest
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 @pytest.fixture(autouse=True)
 def setup(request):
     with open('conf/conf.json') as json_file:
         conf = json.load(json_file)
+        #------------------------------------------------------------------------
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument("--disable-gpu")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--crash-dumps-dir=/tmp')
+        options.add_argument("--start-maximized")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--allow-running-insecure-content')
+        options.add_argument("--incognito")
+        #------------------------------------------------------------------------
+        options.set_capability("acceptInsecureCerts", True)
 
-    # ------------------------------------------------------------------------
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--crash-dumps-dir=/tmp')
-    # options.add_argument("--start-maximized")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument("--incognito")
-    # ------------------------------------------------------------------------
+        desired_capabilities = DesiredCapabilities.CHROME.copy()
+        desired_capabilities['acceptInsecureCerts'] = True
+        driver = webdriver.Chrome(desired_capabilities=desired_capabilities)
+        #------------------------------------------------------------------------
 
-    options.set_capability("acceptInsecureCerts", True)
+        # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
+        # --------------------------------------------------------------------------
+        setup.domain = conf['baseUrl']
+        url = f'{setup.domain}/assets/welcome.html'
+        driver.get(url)
+        #---------------------------------------------------------------------------------
+        print(driver.page_source)
+        print(driver.title)
+        driver.save_screenshot('./save_screenshot_method.png')  # Capture the screen
+        #---------------------------------------------------------------------------------
 
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        request.cls.driver = driver
 
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
-    # --------------------------------------------------------------------------
-
-
-    setup.domain = conf['baseUrl']
-    url = f'{setup.domain}/assets/welcome.html'
-    driver.get(url)
-
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    request.cls.driver = driver
-
-    yield
-    driver.quit()
+        yield
+        driver.quit()
