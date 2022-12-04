f0b0a28212e393a53c456f4f9095eb9d02d6c9a6
SvetlanaKalchenko <116160412+SvetlanaKalchenko@users.noreply.github.com>
2022-12-04 09:49:12 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -13,7 +13,7 @@ def setup(request):
         conf = json.load(json_file)
         #------------------------------------------------------------------------
         options = Options()
-        options.add_argument('--headless')
+        #options.add_argument('--headless')
         options.add_argument("--disable-gpu")
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
